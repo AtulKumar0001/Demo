995e77acc446b4e0d0c9e100b978ccc7661ff6fc
AtulKumar0001 <atul86114@gmail.com>
2023-06-30 14:27:36 +0530

    Initial commit

---
+++ Function/max.js
@@ -0,0 +1,36 @@
+// const number1 = +prompt("Enter number 1");
+// const number2 = +prompt("Enter number 2");
+
+// const sum = function(num1,num2){
+//     if(num1 == ' ' || num2 == ' ') {
+//         alert("The number is invalid");
+//         return 0;
+//     };
+//  alert(`The result is: ${num1+num2}`);
+// }
+// const result = sum(number1,number2);
+
+
+
+
+// Max of three numbers
+// const number1 = +prompt("Enter number 1");
+// const number2 = +prompt("Enter number 2");
+// const number3 = +prompt("Enter number 3");
+
+// const max = function(num1,num2,num3){
+//     if(num1 == ' ' || num2 == ' '|| num3 == ' ') {
+//         alert("The number is invalid");
+//         return 0;+
+//     };
+//     if(num1>num2 && num1>num3){
+//         alert(`Number is greater: ${num1}`);
+//     }
+//     else if(num2>num1 && num2>num3){
+//         alert(`Number is greater: ${num2}`);
+//     }
+//     else{
+//         alert(`Number is greater: ${num3}`);
+//     }
+// }
+// max(number1,number2,number3);
